a498ce1644dbd2e153576591428fa3b7a66c6e1e
axingxing007 <1317462029@qq.com>
2021-04-23 22:59:02 +0800

    modify边界值数据

--- test_interface/test_generate_data.py
+++ test_interface/test_generate_data.py
@@ -6,13 +6,13 @@ class TestGenerateData:
     七点法生成七个边界值数据
     """
 
-    @pytest.mark.parametrize("left, right", [[1, 8], [1, 9]])
-    def test_boundary_data(self, left: int, right: int, pre: int = 1) -> list:
+    @pytest.mark.parametrize("left, right", [[1, 8], [1, 2]])
+    def test_boundary_data(self, left: int, right: int, pre: int = 1) -> set:
         """
         :param left: 左边界，参数的类型为int
         :param right: 右边界，参数的类型为int
         :param pre: 精度，参数的类型为1，默认为1
-        :return: 返回值的类型为list
+        :return: 返回值的类型为set，避免生成重复的数据
         """
         if isinstance(left, int) and isinstance(right, int) and isinstance(pre, int):
             result = []
@@ -22,6 +22,6 @@ class TestGenerateData:
             result.extend(lefts)
             result.append(mid)
             result.extend(rights)
-            return result
+            return set(result)
         else:
             raise ValueError("请检查参数的类型！")
